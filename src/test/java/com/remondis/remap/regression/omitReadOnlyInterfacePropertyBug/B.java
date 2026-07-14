d70aa32125357ae2ebda3ca74e88461ff5a07dab
vanendern <rico.vanendern-ext@remondis.de>
2026-07-14 23:46:21 +0200

    Add the omitInDestination_shouldNotFailOnGetterOnlyInterfaceProperty Test

---
+++ src/test/java/com/remondis/remap/regression/omitReadOnlyInterfacePropertyBug/B.java
@@ -0,0 +1,18 @@
+package com.remondis.remap.regression.omitReadOnlyInterfacePropertyBug;
+
+public class B implements BIface {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getReadOnlyProperty() {
+        return "constant";
+    }
+}
